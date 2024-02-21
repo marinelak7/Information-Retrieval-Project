9453552102543eead4a3bbba615b0b14a7e37c3d
Marinela Kalthi <marinelak@csd.auth.gr>
2024-02-21 12:33:06 +0200

    3d commit

--- data_processing.py
+++ data_processing.py
@@ -2,28 +2,36 @@ from greek_stemmer import stemmer
 from collections import Counter
 
 """
-Returns a list of the Data string with no punctuation and replacing most special characters
+Επιστρέφει μια λίστα της συμβολοσειράς Data χωρίς σημεία στίξης και με αντικατάσταση των περισσότερων ειδικών χαρακτήρων
 """
 def punctuation_removal(Data):
-    preprocessed_data = []
+    preprocessed_data = [] #Αρχικοποίηση μιας λίστας για την αποθήκευση προεπεξεργασμένων δεδομένων
+    
+    # Αφαίρεση σημείων στίξης και αντικατάσταση ειδικών χαρακτήρων στη συμβολοσειρά δεδομένων εισόδου
     preprocessed_data = Data.replace(',', ' ').replace('.', ' ').replace('-', ' ').replace('–', ' ').\
     replace(':', ' ').replace('«', ' ').replace('»', ' ').replace(';', ' ').replace('!', ' ').replace('…',' ').\
     replace('\t', ' ').replace('\b', ' ').replace('\xa0', ' ').replace('έ','ε').replace('ά','α').replace('ή','η').replace('ό','ο').\
     replace('ύ','υ').replace('ί','ι').replace('ώ','ω').replace('Ό','ο').replace('Έ','ε').replace('Ά','α').replace('Ή','η').\
     replace('Ύ','υ').replace('Ί','ι').replace('Ώ','ω').lower()
+    
+    # Διαχωρίστε την προεπεξεργασμένη συμβολοσειρά δεδομένων σε μια λίστα λέξεων
     preprocessed_data = preprocessed_data.split(' ')
+    
+    # Φιλτράρετε τις κενές συμβολοσειρές, τα κενά και τις μη αλφαβητικές λέξεις
     preprocessed_data = [word for word in preprocessed_data if word != '' and word != ' ' and word.isalpha()]
+    
+    
     return preprocessed_data
 
 """
-Returns a joined string of a list of words after removing the stop words from it
+Επιστρέφει μια ενωμένη συμβολοσειρά μιας λίστας λέξεων μετά την αφαίρεση των stopwords
 """
 def stop_word_removal(preprocessed_data, stop_words_array):
-    preprocessed_data1 = []
+    preprocessed_data1 = [] # Αρχικοποίηση μιας κενής λίστας για την αποθήκευση των προεπεξεργασμένων λέξεων μετά την αφαίρεση των λέξεων στάσης
     for word in preprocessed_data:
-        if word not in stop_words_array:
+        if word not in stop_words_array: # Ελέγξτε αν η λέξη δεν βρίσκεται στη λίστα των stop words
             preprocessed_data1.append(word)
-    preprocessed_data1 = ' '.join(preprocessed_data1)                          
+    preprocessed_data1 = ' '.join(preprocessed_data1) # Ενώστε τη λίστα των προεπεξεργασμένων λέξεων χωρίς τις stop words σε ένα ενιαίο αλφαριθμητικό                          
     return preprocessed_data1
 
 """
@@ -34,41 +42,43 @@ def stemming(preprocessed_data):
     index = 0
     preprocessed_data = preprocessed_data.split(' ')
     for word in preprocessed_data:
-        stemmed_word = stemmer.stem_word(word, 'VBG')
-        if stemmed_word.islower():
-            del preprocessed_data[index]
+        stemmed_word = stemmer.stem_word(word, 'VBG') # Get the stemmed version of the word using a stemmer
+        if stemmed_word.islower(): # Check if the stemmed word is in lowercase
+            del preprocessed_data[index] # If the stemmed word is lowercase, delete the corresponding word from the list
         else:
-            preprocessed_data[index] = stemmed_word
+            preprocessed_data[index] = stemmed_word # If the stemmed word is not lowercase, replace the original word with the stemmed word in the list
         index += 1
     preprocessed_data1 = preprocessed_data      
     return preprocessed_data1            
 
 """
-Returns a string after undergoing processing by removing its punctuation and stop words
+Επιστρέφει ένα αλφαριθμητικό μετά από επεξεργασία με αφαίρεση των σημείων στίξης και των stopwords
 """
 def preprocess(Data, stop_words_array):
-    preprocessed_data = stop_word_removal(punctuation_removal(Data), stop_words_array)
+    # Κλήση της συνάρτησης punctuation_removal για την αφαίρεση των σημείων στίξης από την είσοδο Data
+    # Κλήση της συνάρτησης stop_word_removal για την αφαίρεση των stop words από τα δεδομένα χωρίς σημεία στίξης
+    preprocessed_data = stop_word_removal(punctuation_removal(Data), stop_words_array) 
     return preprocessed_data
 
 """
-Returns:
-1. processed_data: a list formed after the Data(speech) list underwent the preprocessing and stemming procedures. Checks if it's empty before returning
-2. tags1: Most common terms in the preprocessed Data(speech)
+Επιστρέφει:
+1. processed_data: μια λίστα που σχηματίζεται αφού η λίστα Data(speech) υποστεί τις διαδικασίες προεπεξεργασίας και στελέχωσης. Ελέγχει αν είναι κενή πριν επιστρέψει
+2. tags1: Οι πιο συνηθισμένοι όροι στην προεπεξεργασμένη λίστα Data(speech)
 """
 def process(Data, stop_words_array):
 
-    preprocessed_data = preprocess(Data, stop_words_array)
+    preprocessed_data = preprocess(Data, stop_words_array) # Προεπεξεργασία των δεδομένων εισόδου και αφαίρεση των σημείων στίξης και των stopwords
     processed_data = stemming(preprocessed_data)
 
-    data_list = preprocessed_data.split(' ')
-    word_frequency = Counter(data_list)
+    data_list = preprocessed_data.split(' ') # Διαχωρισμός των προεπεξεργασμένων δεδομένων σε μια λίστα λέξεων
+    word_frequency = Counter(data_list) # Μετρήστε τη συχνότητα κάθε λέξης στα προεπεξεργασμένα δεδομένα
 
-    tags = word_frequency.most_common(5)
+    tags = word_frequency.most_common(5) # Βρείτε τους 5 πιο συνηθισμένους όρους από το λεξικό συχνότητας λέξεων
     tags1 = []
     for tag in tags:
-        tags1.append(tag[0])
+        tags1.append(tag[0]) # Εξάγετε τους όρους από τις πιο κοινές ετικέτες
     
-    if (processed_data != []):
+    if (processed_data != []): # Ελέγξτε αν τα επεξεργασμένα δεδομένα δεν είναι κενά πριν επιστρέψετε
         return processed_data, tags1
-    else:
+    else: # Επιστρέφει μια τιμή placeholder αν τα επεξεργασμένα δεδομένα είναι κενά
         return 1, 1
--- query.py
+++ query.py
@@ -3,65 +3,98 @@ import initialize as it
 import random as rand
 
 """
-Returns the 5 most similar documents (sittings) to a query:
+Επιστρέφει τα 5 πιο παρόμοια έγγραφα (συνεδριάσεις) σε ένα ερώτημα:
 
-1. sitting ID
-2. name of the speaker
-3. political party of the speaker
-4. tags - most frequent words in the speech
+1. ID συνεδρίασης
+2. όνομα του ομιλητή
+3. πολιτικό κόμμα του ομιλητή
+4. ετικέτες - πιο συχνές λέξεις στην ομιλία
 """
 def get_sittings(query, Data, index_dict, words_dict, tags_dict):
-    similarities = cs.doc_query_similarity(words_dict, query)
+    similarities = cs.doc_query_similarity(words_dict, query) # Υπολογισμός της ομοιότητας μεταξύ του ερωτήματος και κάθε εγγράφου (συνεδρίαση)
     
     sittings = []
     for sitting in similarities:
-        data = [item for sublist in Data.iloc[[index_dict[sitting]], [0, 5]].values.tolist() for item in sublist]
+        data = [item for sublist in Data.iloc[[index_dict[sitting]], [0, 5]].values.tolist() for item in sublist] # Πάρτε τα δεδομένα (ID της συνεδρίασης, όνομα ομιλητή, πολιτικό κόμμα) της συνεδρίασης από το πλαίσιο δεδομένων Data
 
+        # Πάρτε τις πιο συχνές λέξεις (ετικέτες) στην ομιλία της συνεδρίασης από το λεξικό ετικετών
+        # Προσάρτηση πληροφοριών σχετικά με τη συνεδρίαση
         sittings.append([sitting] + data + [' '.join(tags_dict.get(sitting))] + [similarities[sitting]])
     
     return sittings
 
 """
-Returns information about a specific sitting:
+Επιστρέφει πληροφορίες για μια συγκεκριμένη συνεδρίαση:
 
-1. name of the speaker
-2. date the sitting took place
-3. political party of the speaker
-4. speech 
-5. tags - most frequent words in the speech
+1. όνομα του ομιλητή
+2. ημερομηνία διεξαγωγής της συνεδρίασης
+3. πολιτικό κόμμα του ομιλητή
+4. ομιλία 
+5. ετικέτες - πιο συχνές λέξεις στην ομιλία
 """
 def get_sitting_info(sitting_id, Data, index_dict, tags_dict):
+    # Ανάκτηση των σχετικών δεδομένων (όνομα ομιλητή, ημερομηνία, πολιτικό κόμμα, ομιλία) της συνεδρίασης από το πλαίσιο δεδομένων Data
     data = [item for sublist in Data.iloc[[index_dict[int(sitting_id)]], [0, 1, 5, 10] ].values.tolist() for item in sublist]
+    
     
     return data + [' '.join(tags_dict.get(int(sitting_id)))]
 
 """
-Returns all the sittings by a speaker:
+Επιστρέφει όλες τις συνεδριάσεις ενός ομιλητή:
 
-1. sitting ID
-2. political party of the speaker
-3. tags - most frequent words in the speech
+1. ID συνεδρίασης
+2. πολιτικό κόμμα του ομιλητή
+3. ετικέτες - πιο συχνές λέξεις στην ομιλία
 """
 def get_sittings_by_speaker(speaker, Data, index_dict, tags_dict, member_dict):
-    sittings =  [[sitting_id] + get_sitting_info(sitting_id, Data, index_dict, tags_dict)[2::2] for sitting_id in member_dict[speaker]]
-    return sittings[::-1]
+    #sittings =  [[sitting_id] + get_sitting_info(sitting_id, Data, index_dict, tags_dict)[2::2] for sitting_id in member_dict[speaker]]
+    #return sittings[::-1]
+    speaker_sittings_ids = member_dict[speaker]
+    
+    # Αρχικοποίηση μιας κενής λίστας για την αποθήκευση πληροφοριών σχετικά με τις συνεδριάσεις του ομιλητή
+    sittings = []
+    
+    # Επαναλάβετε κάθε αναγνωριστικό συνεδρίασης που σχετίζεται με τον ομιλητή
+    for sitting_id in speaker_sittings_ids:
+        # Λάβετε πληροφορίες σχετικά με τη συνεδρίαση (πολιτικό κόμμα, ετικέτες) χρησιμοποιώντας το αναγνωριστικό συνεδρίασης
+        sitting_info = get_sitting_info(sitting_id, Data, index_dict, tags_dict)
+        
+        # Προσάρτηση πληροφοριών σχετικά με τη συνεδρίαση (ταυτότητα συνεδρίασης, πολιτικό κόμμα, ετικέτες) στον κατάλογο συνεδριάσεων
+        sittings.append([sitting_id] + sitting_info[2::2])
+    
+    # Αντιστροφή της σειράς των συνεδριάσεων (εάν χρειάζεται)
+    sittings = sittings[::-1]
+    
+    # Επιστρέφει τη λίστα των συνεδριάσεων που σχετίζονται με τον ομιλητή
+    return sittings
 
 """
-Returns 5 sittings by a party, each sitting is by a different member:
+Επιστρέφει 5 συνεδριάσεις από ένα κόμμα, κάθε συνεδρίαση από διαφορετικό μέλος:
 
-1. sitting ID
-2. name of the speaker
-3. tags - most frequent words in the speech
+1. ID συνεδρίασης
+2. όνομα του ομιλητή
+3. ετικέτες - πιο συχνές λέξεις στην ομιλία
 """
 def get_sittings_by_party(party, Data, index_dict, tags_dict, party_dict, member_dict):
+    
     sittings = []
+    
+    # Καθορίστε τον μέγιστο αριθμό ομιλητών που θα λάβετε υπόψη (το πολύ 5 ή τον συνολικό αριθμό των ομιλητών του κόμματος)
     max_speakers = 5 if len(party_dict[party])>5 else len(party_dict[party])
     for speaker in party_dict[party][:max_speakers]:
         for sitting_id in member_dict[speaker]:
-            sittings.append([sitting_id] + get_sitting_info(sitting_id, Data, index_dict, tags_dict)[0::4])
+            
+            # Λήψη πληροφοριών σχετικά με τη συνεδρίαση (αναγνωριστικό συνεδρίασης, όνομα ομιλητή, ετικέτες) χρησιμοποιώντας το αναγνωριστικό συνεδρίασης
+            sitting_info = get_sitting_info(sitting_id, Data, index_dict, tags_dict)
+            sittings.append([sitting_id] + sitting_info[0::4])
+            #sittings.append([sitting_id] + get_sitting_info(sitting_id, Data, index_dict, tags_dict)[0::4])
             break
-    return sittings[::-1]
-
+            
+    sittings = sittings[::-1] # Αντιστροφή της σειράς των συνεδριάσεων (εάν χρειάζεται)
+    return sittings
+    #return sittings[::-1]
+    
+    
 
 """# for testing
 if __name__ == "__main__":
